e7348f5d9c73cdaf0c21daa6c2222ae8d20838b9
realmadrid300504 <realmadrid300504@gmail.com>
2023-04-19 11:28:36 +0700

    baru

--- gudang/php/function.php
+++ gudang/php/function.php
@@ -1,23 +1,22 @@
 <?php
 
-$conn = mysqli_connect("localhost","root","","mirorim.v2");
+$conn = mysqli_connect("localhost", "root", "", "mirorim.v2");
 
 //input item
-if(isset($_POST['inputquantityitem'])){
+if (isset($_POST['inputquantityitem'])) {
     $quantity = $_POST['quantity'];
     $list = $_POST['list'];
 
     $jum = count($list);
-    for($i = 0; $i < $jum; $i++){
+    for ($i = 0; $i < $jum; $i++) {
         $update = mysqli_query($conn, "UPDATE item_id SET quantity_count='$quantity[$i]' WHERE id_item='$list[$i]'");
         header('location:?url=addnew');
-    }{
-
+    } {
     }
 }
 
 //input box
-if(isset($_POST['inputquantitybox'])){
+if (isset($_POST['inputquantitybox'])) {
     $quantity = $_POST['totalbox'];
     $kubik = $_POST['totalkubik'];
     $pengiriman = $_POST['pengiriman'];
@@ -25,25 +24,21 @@ if(isset($_POST['inputquantitybox'])){
 
     $jum = count($idb);
     $insert = mysqli_query($conn, "INSERT INTO delivery_id(box_total, kubik_total, pengiriman) VALUES('$quantity','$kubik','$pengiriman')");
-    for($i = 0; $i < $jum; $i++){
-            $select = mysqli_query($conn, "SELECT id_delivery FROM delivery_id WHERE date = (SELECT MAX(date) FROM delivery_id)");
-            $data = mysqli_fetch_array($select);
-            $idd = $data['id_delivery'];
-            if($select){
-                $update = mysqli_query($conn, "UPDATE boxorder_id SET id_delivery='$idd' WHERE id_box='$idb[$i]'");
-                header('location:?url=boxlist');
-            } else {
-
-            }
-    }{
-
+    for ($i = 0; $i < $jum; $i++) {
+        $select = mysqli_query($conn, "SELECT id_delivery FROM delivery_id WHERE date = (SELECT MAX(date) FROM delivery_id)");
+        $data = mysqli_fetch_array($select);
+        $idd = $data['id_delivery'];
+        if ($select) {
+            $update = mysqli_query($conn, "UPDATE boxorder_id SET id_delivery='$idd' WHERE id_box='$idb[$i]'");
+            header('location:?url=boxlist');
+        } else {
+        }
+    } {
     }
-
-    
 }
 
 //Edit No Sku
-if(isset($_POST['editnonsku'])){
+if (isset($_POST['editnonsku'])) {
     $idp = $_POST['idp'];
     $skug = $_POST['skug'];
     $quantity = $_POST['quantity'];
@@ -52,94 +47,86 @@ if(isset($_POST['editnonsku'])){
     $selectskug = mysqli_query($conn, "SELECT * FROM gudang_id WHERE sku_gudang='$skug'");
     $sku = mysqli_num_rows($selectskug);
 
-    if($sku=='0'){
+    if ($sku == '0') {
         $insert = mysqli_query($conn, "INSERT INTO gudang_id(id_product, sku_gudang, lokasi_gudang, quantity) VALUES('$idp','$skug','$gudang','$quantity')");
-        if($insert){
+        if ($insert) {
             $select = mysqli_query($conn, "SELECT * FROM item_id WHERE id_product='$idp'");
             $data = mysqli_fetch_array($select);
             $quantityitem = $data['quantity_count'];
 
-            $kurang = $quantityitem-$quantity;
-            if($select){
+            $kurang = $quantityitem - $quantity;
+            if ($select) {
                 $update = mysqli_query($conn, "UPDATE item_id SET quantity_count='$kurang' WHERE id_product='$idp'");
                 header('location:?url=gudang');
             } else {
-
             }
         } else {
-
         }
     } else {
         $ambilqty = mysqli_query($conn, "SELECT * FROM gudang_id WHERE sku_gudang='$skug'");
         $data = mysqli_fetch_array($ambilqty);
         $qty = $data['quantity'];
 
-        $tambah  = $qty+$quantity;
-        if($ambilqty){
+        $tambah  = $qty + $quantity;
+        if ($ambilqty) {
             $update = mysqli_query($conn, "UPDATE gudang_id SET quantity='$tambah' WHERE sku_gudang='$skug'");
-            if($update){
+            if ($update) {
                 $select = mysqli_query($conn, "SELECT * FROM item_id WHERE id_product='$idp'");
                 $data = mysqli_fetch_array($select);
                 $quantityitem = $data['quantity_count'];
-    
-                $kurang = $quantityitem-$quantity;
-                if($select){
+
+                $kurang = $quantityitem - $quantity;
+                if ($select) {
                     $update = mysqli_query($conn, "UPDATE item_id SET quantity_count='$kurang' WHERE id_product='$idp'");
                     header('location:?url=gudang');
                 } else {
-    
                 }
             } else {
-    
             }
         } else {
-
         }
     }
 }
 
 //Edit Sku
-if(isset($_POST['editsku'])){
+if (isset($_POST['editsku'])) {
     $idp = $_POST['idp'];
     $gudang = $_POST['gudang'];
 
-        $updateskug = mysqli_query($conn, "UPDATE gudang_id SET gudang='$gudang' WHERE id_product='$idp'");
-        if($updateskug){
-            header('location:?url=gudang');
-        } else {
-
-        }
+    $updateskug = mysqli_query($conn, "UPDATE gudang_id SET gudang='$gudang' WHERE id_product='$idp'");
+    if ($updateskug) {
+        header('location:?url=gudang');
+    } else {
+    }
 }
 
 //Ceklist Toko
-if(isset($_POST['cektoko'])){
+if (isset($_POST['cektoko'])) {
     $idt = $_POST['cek'];
     $quantity = $_POST['quantity'];
     $stat = $_POST['stat'];
     $idg = $_POST['idg'];
     $picker = $_POST['picker'];
-    
+
 
     $jum = count($idt);
-    for($i = 0; $i < $jum; $i++){
-            $select = mysqli_query($conn, "SELECT * FROM request_id WHERE id_request='$idt[$i]'");
-            $data = mysqli_fetch_array($select);
-            $qty = $data['quantity_req'];
-            if($qty==0){
-                $update = mysqli_query($conn, "UPDATE request_id SET quantity_req='$quantity[$i]', status_req='$stat', id_gudang='$idg[$i]', picker='$picker' WHERE id_request='$idt[$i]'");
-                header('location:?url=exititem');
-            } else {
-                $update = mysqli_query($conn, "UPDATE request_id SET status_req='$stat', id_gudang='$idg[$i]', picker='$picker' WHERE id_request='$idt[$i]'");
-                header('location:?url=exititem');
-            }
-        
+    for ($i = 0; $i < $jum; $i++) {
+        $select = mysqli_query($conn, "SELECT * FROM request_id WHERE id_request='$idt[$i]'");
+        $data = mysqli_fetch_array($select);
+        $qty = $data['quantity_req'];
+        if ($qty == 0) {
+            $update = mysqli_query($conn, "UPDATE request_id SET quantity_req='$quantity[$i]', status_req='$stat', id_gudang='$idg[$i]', picker='$picker' WHERE id_request='$idt[$i]'");
+            header('location:?url=exititem');
+        } else {
+            $update = mysqli_query($conn, "UPDATE request_id SET status_req='$stat', id_gudang='$idg[$i]', picker='$picker' WHERE id_request='$idt[$i]'");
+            header('location:?url=exititem');
+        }
     } {
-
     }
 }
 
 //Request Prepare
-if(isset($_POST['preparereq'])){
+if (isset($_POST['preparereq'])) {
     $idp = $_POST['idp'];
     $idg = $_POST['idg'];
     $idk = $_POST['idk'];
@@ -150,39 +137,37 @@ if(isset($_POST['preparereq'])){
     $typereq = $_POST['typereq'];
     $jum = $_POST['jum'];
 
-    for($i = 0; $i < $jum; $i++){
+    for ($i = 0; $i < $jum; $i++) {
         $select1 = mysqli_query($conn, "SELECT * FROM list_komponen WHERE id_product_finish='$idp[$i]'");
         $data1 = mysqli_fetch_array($select1);
         $idkomp = $data1['id_komponen'];
 
 
-        if($select1){
+        if ($select1) {
             $insert = mysqli_query($conn, "INSERT INTO request_prepare(id_product_finish, quantity_req, status_prepare, gudang_out, type_req, requester) VALUES('$idp[$i]','$quantity[$i]','$stat','$idg[$i]','$typereq[$i]','$requester')");
-            if($insert){
+            if ($insert) {
                 $select2 = mysqli_query($conn, "SELECT * FROM gudang_id WHERE id_gudang='$idk[$i]'");
                 $data2 = mysqli_fetch_array($select2);
                 $quantityril = $data2['quantity'];
 
-                $kurang = $quantityril-$quantity2[$i];
-                if($select2){
+                $kurang = $quantityril - $quantity2[$i];
+                if ($select2) {
                     $update = mysqli_query($conn, "UPDATE gudang_id SET quantity='$kurang' WHERE id_gudang='$idk[$i]'");
                     header('location:?url=exitprepare');
                 } else {
                     echo "Lohh";
-                }   
+                }
             } else {
-
             }
         } else {
             echo "Gagal";
         }
-    }{
-
+    } {
     }
 }
 
 //Acc Broo
-if(isset($_POST['komponenacc'])){
+if (isset($_POST['komponenacc'])) {
     $idp = $_POST['idp'];
     $cek = $_POST['cek'];
     $stat = $_POST['stat'];
@@ -192,31 +177,27 @@ if(isset($_POST['komponenacc'])){
     $date = date('Y-m-d H:i:s');
 
     $jum = count($cek);
-    for ($i = 0; $i < $jum; $i++){
+    for ($i = 0; $i < $jum; $i++) {
         $select = mysqli_query($conn, "SELECT quantity FROM gudang_id WHERE id_gudang='$idg[$i]'");
         $data = mysqli_fetch_array($select);
         $quantity1 = $data['quantity'];
 
-        $tambah = $quantity1+$quantity[$i];
-        if($select){
+        $tambah = $quantity1 + $quantity[$i];
+        if ($select) {
             $update = mysqli_query($conn, "UPDATE request_prepare SET gudang_in='$idg[$i]', status_prepare='$stat' WHERE id_prepare='$cek[$i]'");
-            if($update){
+            if ($update) {
                 $update1 = mysqli_query($conn, "UPDATE gudang_id SET quantity='$tambah' WHERE id_gudang='$idg[$i]'");
-                header('location:?url=exitprepare'); 
+                header('location:?url=exitprepare');
             } else {
-
             }
         } else {
-
         }
-       
-    }{
-
+    } {
     }
 }
 
 //mutasi
-if(isset($_POST['mutasi'])){
+if (isset($_POST['mutasi'])) {
     $skug = $_POST['skug'];
     $idg = $_POST['idg'];
     $quantity = $_POST['quantity'];
@@ -224,69 +205,61 @@ if(isset($_POST['mutasi'])){
     $skug1 = $_POST['skug1'];
 
     $jum = count($skug);
-    for($i = 0; $i < $jum; $i++){
+    for ($i = 0; $i < $jum; $i++) {
         $select = mysqli_query($conn, "SELECT quantity, sku_gudang, id_product, lokasi_gudang FROM gudang_id WHERE id_gudang='$idg[$i]'");
         $data = mysqli_fetch_array($select);
         $idp = $data['id_product'];
         $lok = $data['lokasi_gudang'];
 
-        if($quantity==$quantity1){
-            if($skug==$skug1){
-                
+        if ($quantity == $quantity1) {
+            if ($skug == $skug1) {
             } else {
                 $insert = mysqli_query($conn, "INSERT INTO mutasi_id(id_gudang, skug_lama, skug_baru, quantity) VALUES('$idg[$i]','$skug1[$i]','$skug[$i]','$quantity[$i]')");
                 header('location:?url=mutasi');
             }
         } else {
-            $selectnum = mysqli_query($conn, "SELECT sku_gudang FROM gudang_id");
+            $selectnum = mysqli_query($conn, "SELECT sku_gudang FROM gudang_id WHERE sku_gudang='$skug[$i]'");
             $hitung = mysqli_num_rows($selectnum);
-            if($hitung>0){
-                $tambah = $quantity1[$i]+$quantity[$i];
+            if ($hitung > 0) {
+                $tambah = $quantity1[$i] + $quantity[$i];
                 $update1 = mysqli_query($conn, "UPDATE gudang_id SET quantity='$tambah' WHERE sku_gudang='$skug[$i]'");
                 header('location:?url=mutasi');
             } else {
-           $insert = mysqli_query($conn, "INSERT INTO gudang_id(id_product, sku_gudang, quantity) VALUES('$idp','$skug[$i]','$quantity[$i]')");
-           if($insert){
-            $kurang = $quantity1[$i]-$quantity[$i];
-            $update = mysqli_query($conn, "UPDATE gudang_id SET quantity='$kurang' WHERE id_gudang='$idg[$i]'");
-            header('location:?url=mutasi');
-           }else {
-
-           }
+                $insert = mysqli_query($conn, "INSERT INTO gudang_id(id_product, sku_gudang, quantity) VALUES('$idp','$skug[$i]','$quantity[$i]')");
+                if ($insert) {
+                    $kurang = $quantity1[$i] - $quantity[$i];
+                    $update = mysqli_query($conn, "UPDATE gudang_id SET quantity='$kurang' WHERE id_gudang='$idg[$i]'");
+                    header('location:?url=mutasi');
+                } else {
+                }
+            }
         }
-        }
-    }{
-
+    } {
     }
 }
 
 //mutasi acc
-if(isset($_POST['mutasiacc'])){
+if (isset($_POST['mutasiacc'])) {
     $cek = $_POST['cek'];
     $stat = $_POST['stat'];
     $idg = $_POST['idg'];
     $idm = $_POST['idm'];
 
     $jum = count($cek);
-    for ($i = 0; $i < $jum; $i++){
+    for ($i = 0; $i < $jum; $i++) {
         $select = mysqli_query($conn, "SELECT * FROM mutasi_id WHERE id_mutasi='$idmm[$i]'");
         $data = mysqli_fetch_array($select);
         $skug = $data['skug_baru'];
 
-        if($select){
+        if ($select) {
             $update = mysqli_query($conn, "UPDATE gudang_id SET sku_gudang='$skug' WHERE id_gudang='$idg[$i]'");
-            if($update){
+            if ($update) {
                 $update1 = mysqli_query($conn, "UPDATE mutasi_id SET status_mutasi='$stat' WHERE id_mutasi='$idm[$i]'");
-                header('location:?url=approvemutasi'); 
+                header('location:?url=approvemutasi');
             } else {
-
             }
         } else {
-
         }
-       
-    }{
-
+    } {
     }
 }
-?>
--- preparation/addkomp.php
+++ preparation/addkomp.php
@@ -7,78 +7,60 @@ $data = mysqli_fetch_array($select);
 ?>
 
 <div class="row">
-                    <div class="card">
-                        <div class="card-body">
-                        <h5 class="card-title">Check Packing List</h5>
-                        <!--Submit-->
-                        <!-- Begin Page Content -->
-                <div class="container-fluid">
-                            <hr>
-                            <form method="post" action="">
-                            <div class="row">
-                                <div class="col-md-4">
-                                <div class="form-group">
-                                    <label for="nama">Quantity Variant</label>
-                                    <input class="form-control" name="jum" id="nama" type="number" require>
-                                </div>
-                                </div>
-                                <div class="col-md-6">
-                                <div class="form-group">
-                                    <label for="nama">///////</label>
-                                    <br>
-                                    <button class="btn btn-outline-primary" type="submit" action="" name="qtyvariant">Submit</button>
-                                </div>
-                                </div>
-                            </form>
-                                <div class="col-md-4">
-                                <div class="form-group">
-                                    <label for="nama">Item Name</label>
-                                    <input class="form-control" readonly name="nama" id="nama" value="<?=$data['nama'];?>" type="text" require>
-                                </div>
-                                </div>
-                            </div>
-                            <hr>
-                            <form method="post">
-                            <div class="table-responsive">
-                                    <table class="table table-hover table-bordered" width="100%" cellspacing="0">
-                                        <thead>
-                                            <tr>
-                                                <th>No</th>
-                                                <th>Nama</th>
-                                                <th>Quantity</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                        <?php
-                                         if(isset($_POST['qtyvariant'])){
-                                                $s = 1;
-                                                $jum = $_POST['jum'];
-
-                                                for($i=0; $i < $jum; $i++){
-                                            ?>
-                                            <tr>
-                                                <th><?=$s++;?></th>
-                                                <td><input type="text" class="form-control" name="nama[]" require=""></td>
-                                                    
-                                                </td>
-                                                <td>
-                                                    <input type="number" class="form-control" name="quantity[]" require="">
-                                                    <input type="hidden" name="jum[]" value="<?=$jum;?>">
-                                                    <input type="hidden" name="idp" value="<?=$idp;?>">
-                                                </td>
-                                            </tr>
-                                            <?php
-                                                }}
-
-                                            ?>
-                                            </tbody>
-                                    </table>
-                                    <div class="text-right">
-                                        <button type="submit" class="btn btn-primary" name="komponeninput">Submit</button>
-                                    </div>
-                                </div>
-                            </form>
-            
+    <div class="card">
+        <div class="card-body">
+            <h5 class="card-title">Check Packing List</h5>
+            <!--Submit-->
+            <!-- Begin Page Content -->
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <label for="nama">Item Name</label>
+                            <input class="form-control" readonly name="nama" id="nama" value="<?= $data['nama']; ?>" type="text" require>
                         </div>
                     </div>
+                </div>
+                <hr>
+                <form method="post">
+                    <div class="table-responsive">
+                        <table class="table table-hover table-bordered" id="dataTable" width="100%" cellspacing="0">
+                            <thead>
+                                <tr>
+                                    <th>No</th>
+                                    <th>Nama</th>
+                                    <th>Quantity</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                $select = mysqli_query($conn, "SELECT * FROM product_id WHERE jenis='Mentah'");
+                                $s = 1;
+                                while ($data = mysqli_fetch_array($select)) {
+                                ?>
+                                    <tr>
+                                        <th><?= $s++; ?></th>
+                                        <td><?= $data['nama']; ?></td>
+
+                                        </td>
+                                        <td>
+                                            <input type="number" class="form-control" name="quantity[]" require="">
+                                            <input type="hidden" name="jum[]" value="<?= $jum; ?>">
+                                            <input type="hidden" name="idp" value="<?= $idp; ?>">
+                                        </td>
+                                    </tr>
+                                <?php
+                                }
+
+                                ?>
+                            </tbody>
+                        </table>
+                        <div class="text-right">
+                            <button type="submit" class="btn btn-primary" name="komponeninput">Submit</button>
+                        </div>
                     </div>
+                </form>
+
+            </div>
+        </div>
+    </div>
